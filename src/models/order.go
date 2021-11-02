7eea80868cdc565ac346effbc50945d7788e6db8
ArthurDev <arthur.quezada98@gmail.com>
2021-11-02 16:24:50 -0500

    add get customer

--- src/models/order.go
+++ src/models/order.go
@@ -8,11 +8,11 @@ import (
 
 type Order struct {
 	ID    primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	User  primitive.ObjectID `bson:"userId" json:"userId"`
-	State string             `bson:"state" json:"state"`
+	User  primitive.ObjectID `bson:"userId,omitempty" json:"userId"`
+	State string             `bson:"state" json:"state,omitempty"`
 	Items struct {
 		Product  []primitive.ObjectID `bson:"products" json:"products"`
 		Quantity int                  `bson:"quantity" json:"quantity"`
 	}
-	Date time.Time `bson:"date" json:"date,omitempty"`
+	Date time.Time `bson:"date" json:"date"`
 }
---
+++ src/services/readCustomer.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/ArthurQR98/e-commerce/config"
+	"github.com/ArthurQR98/e-commerce/src/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func GetCustomers(paginate int64) ([]*models.Customer, bool) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	db := config.MongoCN.Database("ecommerce")
+	col := db.Collection("customers")
+
+	var results []*models.Customer
+	option := options.Find()
+	option.SetLimit(20)
+	option.SetSkip((paginate - 1) * 20)
+
+	cursor, err := col.Find(ctx, bson.M{}, option)
+	if err != nil {
+		log.Fatal(err.Error())
+		return results, false
+	}
+	for cursor.Next(context.TODO()) {
+		var register models.Customer
+		err := cursor.Decode(&register)
+		if err != nil {
+			return results, false
+		}
+		results = append(results, &register)
+	}
+	return results, true
+}
